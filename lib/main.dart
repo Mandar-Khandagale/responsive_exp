77e5a298b037a1e68e170f6cda6a4e287f4772a1
Mandar Khandagale <mandar.k@imimobile.com>
2023-04-03 16:14:50 +0530

    added signing config and build release settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_flutter_app/demo_ui.dart';
 import 'package:responsive_flutter_app/layout_builder/layout_builder_exp.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:  const OrientationBuilderExp(),
+      home:  const DemoUI(),
     );
   }
 }
